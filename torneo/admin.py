488c7c7acec0e228b429ea1902edf29b71abbe79
Kevin Schiebelbein <kevin.schiebelbein@gmail.com>
2022-02-19 09:50:14 -0300

    Configuracion de subida de imagenes y clase meta en modelo Temporada

--- torneo/admin.py
+++ torneo/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Temporada
 
 # Register your models here.
-admin.site.register(Temporada)
+
+class TemporadaAdmin(admin.ModelAdmin):
+  readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Temporada, TemporadaAdmin)
--- torneo/models.py
+++ torneo/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -5,6 +7,15 @@ class Temporada(models.Model):
   name = models.CharField(verbose_name="Nombre de temporada", max_length=128)
   description = models.TextField(verbose_name="Descripcion")
   start_day = models.DateField(verbose_name="Fecha de inicio")
+  logo = models.ImageField(default='null', upload_to="temporadas")
   updated_at = models.DateTimeField(auto_now=True, verbose_name="Ultima actualizacion")
   created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
   active = models.BooleanField(verbose_name="Activo")
+
+  class Meta:
+    verbose_name = "Temporada"
+    verbose_name_plural = "Temporadas"
+    ordering = ['-id']
+
+  def __str__(self) -> str:
+      return f"{self.name}"
