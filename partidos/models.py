60e80033e47bf247cd5dd99b95078ceb1a097948
Kevin Schiebelbein <kevinschiebelbein@MacBook-Pro-de-Kevin.local>
2022-03-01 12:19:16 -0300

    Modelo de Partido

--- partidos/models.py
+++ partidos/models.py
@@ -1,21 +1,30 @@
+from tabnanny import verbose
 from django.db import models
 from equipos.models import Equipo
-
+from torneo.models import Cancha
 #Create your models here. 
 
-class Jugador(models.Model):
-  name = models.CharField(verbose_name="Nombre", max_length=16)
-  last_name = models.CharField(verbose_name="Apellido",max_length=16)
-  email = models.CharField(verbose_name="Email",max_length=64)
+class Partido(models.Model):
+  date = models.DateField(verbose_name="Dia")
+  time = models.TimeField(verbose_name="Hora")
+  local = models.ForeignKey(Equipo, on_delete=models.SET_NULL, null=True, verbose_name="Local")
+  #visit = models.ForeignKey(Equipo, on_delete=models.SET_NULL, null=True, verbose_name="Visitante")
+  stadium = models.ForeignKey(Cancha, on_delete=models.SET_NULL, null=True, verbose_name="Cancha")
+  STATUS_MATCH = [
+    ('PD', 'Pendiente'),
+    ('SP', 'Suspendido'),
+    ('TR', 'Terminado')
+  ]
+  status = models.CharField(max_length=2, choices=STATUS_MATCH, verbose_name="Estado del partido")
   alias = models.CharField(verbose_name="Alias",max_length=16)
   updated_at = models.DateTimeField(auto_now=True, verbose_name="Ultima actualizacion")
   created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
   active = models.BooleanField(verbose_name="Activo")
 
   class Meta:
-    verbose_name = "Jugador"
-    verbose_name_plural = "Jugadores"
-    ordering = ['-name']
+    verbose_name = "Partido"
+    verbose_name_plural = "Partidos"
+    ordering = ['-date']
 
   def __str__(self) -> str:
     return f"{self.name} {self.last_name}"
